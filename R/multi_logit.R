8f4b749c3b97e526eb6b0437c5cb0f41f0abcc52
yywwwyy <3173231486@qq.com>
2023-12-10 19:03:58 -0500

    edit multi_logit

--- R/multi_logit.R
+++ R/multi_logit.R
@@ -4,15 +4,13 @@
 #' Multinomial Logistic Regression for analysis.
 #' @param d cleaned table for analysis
 #' @return a data frame with the
-#' @importFrom purrr
-#' @importFrom dplyr
+#' @importFrom gtsummary tbl_regression
+#' @importFrom generics tidy
 #' @importFrom nnet multinom
 #' @export
 mult_logit = function(d){
+  model <- multinom(BEST ~ ATRT+TYPE+SEX+AGE, data = d)
 
-  model <- multinom(BEST ~ ATRT + TYPE + SEX + AGE + B_WEIGHT + B_HEIGHT + RACE, data = d)
-
-  tidy(model, conf.int = TRUE, exponentiate = TRUE) |>
-    kable() |>
-    kable_styling("basic", full_width = FALSE)
+  #tbl_regression(model, exp = TRUE)
+  tidy(model, conf.int = TRUE)
 }
