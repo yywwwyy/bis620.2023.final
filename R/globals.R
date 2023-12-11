2c727cad4b5001fdced7accd328e018cfb0b0da8
yywwwyy <3173231486@qq.com>
2023-12-10 19:31:09 -0500

    change import

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,1 @@
-utils::globalVariables(c("X", "Y", "Z", "Acceleration"))
+utils::globalVariables(c("X", "Y", "Z", "Acceleration",'BMMTR1','BMMTR2','BMMTR3','BMMTR15','SUBJID','RSRESP','BEST','count','TYPE','Mutant','Wild_type','Unknown','name','value'))
--- R/multi_logit.R
+++ R/multi_logit.R
@@ -4,13 +4,11 @@
 #' Multinomial Logistic Regression for analysis.
 #' @param d cleaned table for analysis
 #' @return a data frame with the logit result
-#' @importFrom gtsummary tbl_regression
 #' @importFrom generics tidy
 #' @importFrom nnet multinom
 #' @export
 mult_logit = function(d){
   model <- multinom(BEST ~ ATRT+TYPE+SEX+AGE, data = d)
 
-  #tbl_regression(model, exp = TRUE)
   return(tidy(model, conf.int = TRUE))
 }
